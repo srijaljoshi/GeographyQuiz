a211026711bae44444810b1e036e7a1f5296402b
Tripp <tripp.guinn@outlook.com>
2019-03-27 17:31:31 -0400

    Quiz Activity and Fragment
    Added QuizActivity and QuizFragment Classes

---
+++ app/src/main/java/edu/uga/cs6060/geographyquiz/QuizActivity.java
@@ -0,0 +1,13 @@
+package edu.uga.cs6060.geographyquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/edu/uga/cs6060/geographyquiz/QuizFragment.java
@@ -0,0 +1,7 @@
+package edu.uga.cs6060.geographyquiz;
+
+import android.support.v4.app.Fragment;
+
+public class QuizFragment extends Fragment {
+    
+}
