24adf7281289ba44912339f8a93152a1a5d35385
Siraaj Fadoo <Siraaj@MacBook-Air.local>
2019-04-01 00:56:58 -0400

    more screens

---
+++ app/src/main/java/edu/uga/cs6060/geographyquiz/BasicQuizQuestionFragment.java
@@ -0,0 +1,12 @@
+package edu.uga.cs6060.geographyquiz;
+
+import android.support.v4.app.Fragment;
+
+public class BasicQuizQuestionFragment extends Fragment {
+
+
+
+
+
+
+}
--- app/src/main/java/edu/uga/cs6060/geographyquiz/StartQuizActivity.java
+++ app/src/main/java/edu/uga/cs6060/geographyquiz/StartQuizActivity.java
@@ -1,13 +1,37 @@
 package edu.uga.cs6060.geographyquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StartQuizActivity extends AppCompatActivity {
+
+    Button basicQuiz;
+    Button advancedQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_quiz);
+
+        basicQuiz = findViewById( R.id.button3 );
+        basicQuiz.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), BasicQuizQuestion1Activity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+//        advancedQuiz = findViewById( R.id.button4 );
+//        advancedQuiz.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), AdvancedQuizQuestion1Activity.class);
+//                v.getContext().startActivity(intent);
+//            }
+//        });
     }
+
+
 }
